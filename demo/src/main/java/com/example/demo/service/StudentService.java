faf377572f079721f2a7f53178118e463f589ca2
Kim Dequilla <67781364+dequilla3@users.noreply.github.com>
2022-01-09 15:04:14 +0800

    added updatedStudent method

--- demo/src/main/java/com/example/demo/service/StudentService.java
+++ demo/src/main/java/com/example/demo/service/StudentService.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +32,30 @@ public class StudentService {
   }
 
   public void deleteStudent(Long studentId) {
-    Boolean isExist = studentRepository.existsById(studentId);
+    boolean isExist = studentRepository.existsById(studentId);
+    if (!isExist) {
+      throw new RuntimeException("Student ID: " + studentId + " does not exists!");
+    }
+    studentRepository.deleteById(studentId);
+  }
+
+  @Transactional
+  public void updateStudent(Long studentId, String name, String email) {
+    Student student =
+        studentRepository
+            .findById(studentId)
+            .orElseThrow(() -> new RuntimeException("Student does not exist"));
+
+    if (name !=null &&!name.isEmpty() && !name.equalsIgnoreCase(student.getName())) {
+      student.setName(name);
+    }
+
+    if (email !=null && !email.isEmpty() && !email.equals(student.getEmail())) {
+      Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+      if (studentOptional.isPresent()) {
+        throw new RuntimeException("Email is taken");
+      }
+      student.setEmail(email);
+    }
   }
 }
